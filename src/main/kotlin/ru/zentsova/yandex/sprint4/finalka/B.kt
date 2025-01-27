1eb17099e7464f5cab71451f60f3980f9a68170f
Anastasiia Zentsova <pestovnikova2013@gmail.com>
2025-01-28 00:13:39 +0300

    sprint 4 - finalka B

--- src/main/kotlin/ru/zentsova/yandex/sprint4/finalka/B.kt
+++ src/main/kotlin/ru/zentsova/yandex/sprint4/finalka/B.kt
@@ -1,38 +1,102 @@
 package ru.zentsova.yandex.sprint4.finalka
 
-import ru.zentsova.yandex.sprint4.finalka.Operation.PUT
-import ru.zentsova.yandex.sprint4.finalka.Operation.GET
-import ru.zentsova.yandex.sprint4.finalka.Operation.DELETE
 import java.io.BufferedReader
 import java.util.*
+import kotlin.math.abs
 
-class HashTable<K, V>() {
-	fun put(key: K, value: V) {
+class HashTable(private val size: Int = 99991) {
+	private val table = Array<MutableList<Pair1>?>(size) { null }
 
+	fun put(key: Int, value: Int) {
+		val hashCode = hashCode(key)
+		val idx = mod(hashCode, size)
+		val list = table[idx]
+
+		var shouldAdd = true
+		if (list != null) {
+			for (i in list.indices) {
+				if (list[i].key == key) {
+					list[i].value = value
+					shouldAdd = false
+					break
+				}
+			}
+			if (shouldAdd) table[idx]!!.add(Pair1(key, value))
+		} else {
+			table[idx] = mutableListOf(Pair1(key, value))
+		}
 	}
 
-	fun get(key: K): V? {
+	fun get(key: Int): Int? {
+		val hashCode = hashCode(key)
+		val idx = mod(hashCode, size)
+		val list = table[idx]
 
-		return null
+		return if (list != null) {
+			var value: Int? = null
+			for (i in list.indices) {
+				if (list[i].key == key) {
+					value = list[i].value
+					break
+				}
+			}
+			value
+		} else null
 	}
 
-	fun delete(k: K): V? {
+	fun delete(key: Int): Int? {
+		val hashCode = hashCode(key)
+		val idx = mod(hashCode, size)
+		val list = table[idx]
 
-		return null
+		return if (list != null) {
+			var idxToDelete = -1
+			for (i in list.indices) {
+				if (list[i].key == key) {
+					idxToDelete = i
+					break
+				}
+			}
+
+			val valueToReturn = if (idxToDelete != -1) {
+				val value = list[idxToDelete].value
+				list.removeAt(idxToDelete)
+				value
+			} else null
+
+			valueToReturn
+		} else null
 	}
 
-	private fun hashCode(k: K): Int {
-
-		return 0
+	private fun hashCode(k: Int): Int {
+		return k
 	}
+
+	private fun mod(x: Int, y: Int): Int {
+		val j = when {
+			x < 0 -> -1 * (abs(x) / y + 1)
+			else -> x / y
+		}
+		return x - j
+	}
+
+	private data class Pair1(
+		val key: Int,
+		var value: Int,
+	)
 }
 
 fun main() {
 	val reader = System.`in`.bufferedReader()
   val operationsCount = reader.readInt()
-	val hashTable = HashTable<Int, Int>()
-	for (i in 0..< operationsCount) {
+	val hashTable = HashTable()
+	for (i in 0 until operationsCount) {
 		val (operation, key, value) = reader.command()
+		when (operation) {
+			Operation.PUT -> hashTable.put(key, value!!)
+			Operation.GET -> hashTable.get(key)?.let { println(it) } ?: println("None")
+			Operation.DELETE -> hashTable.delete(key)?.let { println(it) } ?: println("None")
+		}
 	}
 }
 
@@ -49,15 +113,12 @@ private fun BufferedReader.command(): Triple<Operation, Int, Int?> {
 }
 
 private fun String.toOperation(): Operation = when (this) {
-	"put" -> PUT
-	"get" -> GET
-	"delete" -> DELETE
+	"put" -> Operation.PUT
+	"get" -> Operation.GET
+	"delete" -> Operation.DELETE
 	else -> throw IllegalArgumentException("Unknown operation")
 }
 
 enum class Operation {
-	PUT,
-	GET,
-	DELETE
+	PUT, GET, DELETE,
 }
-
