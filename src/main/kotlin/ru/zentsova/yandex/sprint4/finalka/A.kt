acc79dc2501142db375b558e814b119a849a6e5d
Anastasiia Zentsova <pestovnikova2013@gmail.com>
2025-02-03 07:44:07 +0300

    sprint 4 - finalka A, B

--- src/main/kotlin/ru/zentsova/yandex/sprint4/finalka/A.kt
+++ src/main/kotlin/ru/zentsova/yandex/sprint4/finalka/A.kt
@@ -4,77 +4,66 @@ import java.io.BufferedReader
 
 fun main() {
 	val reader = System.`in`.bufferedReader()
-	val documentsCount = reader.readInt()
-	// индексы не важны?
-	// val searchIndex = mutableMapOf<String, Int>()
-	val inputDocuments = (1..documentsCount).map {
-		countForSameWordsInLine(reader.readLine())
+
+	val searchIndex = mutableMapOf<String, MutableMap<Int, Int>>()
+	repeat(reader.readInt()) { idx ->
+		searchIndex(
+			document = reader.readLine(),
+			docSerialNum = idx + 1,
+			searchIndex = searchIndex
+		)
 	}
 
-	val queriesCount = reader.readInt()
-	val inputQueries = (1..queriesCount).map { reader.readLine() }
+	val inputQueries = (1..reader.readInt()).map { reader.readLine() }
+	val queryDocumentRelevance = documentRelevance(queries = inputQueries, searchIndex = searchIndex)
+
+  searchIndex.clear()
+
+	val outputBuffer = StringBuilder()
+	for (query in queryDocumentRelevance) {
+		query.sort()
+			.let { if (it.size < 5) it else it.subList(0, 5) }
+			.forEach { outputBuffer.append(it.serialNumber).append(" ") }
+
+		outputBuffer.append("\n")
+	}
+	println(outputBuffer)
+}
+
+fun searchIndex(document: String, docSerialNum: Int, searchIndex: MutableMap<String, MutableMap<Int, Int>>) {
+	document.split(" ").forEach { word ->
+		val wordsData = searchIndex.getOrPut(word) { mutableMapOf() }
+		wordsData[docSerialNum] = wordsData.getOrDefault(docSerialNum, 0) + 1
+	}
+}
+
+fun documentRelevance(queries: List<String>, searchIndex: Map<String, Map<Int, Int>>): List<List<DocumentData>> {
+	if (searchIndex.isEmpty() || queries.isEmpty()) return emptyList()
+
+	val result = mutableListOf<List<DocumentData>>()
+	queries.forEach { query ->
+		val queryDocumentData = mutableMapOf<Int, Int>()
+		query.split(" ").toSet().forEach { word ->
+			if (searchIndex.containsKey(word)) {
+				searchIndex.getValue(word).forEach { (serialDocNum, count) ->
+					queryDocumentData.compute(serialDocNum) { _, curCount -> curCount?.let { it + count } ?: count }
+				}
+			}
+		}
+		val documentDataList = queryDocumentData.map { DocumentData(it.key, it.value) }
+		result.add(documentDataList)
+	}
+
+	return result
 }
 
 private fun BufferedReader.readInt() = readLine().toInt()
 
-fun documentRelevance(queries: List<String>, documents: List<Map<String, Int>>): List<Map<Int, Int>> {
-	if (documents.isEmpty() || queries.isEmpty()) return emptyList()
+private fun List<DocumentData>.sort() = sortedWith(
+	compareByDescending<DocumentData> { it.relevanceValue }.thenBy { it.serialNumber }
+)
 
-	val result = MutableList<MutableMap<Int, Int>>(queries.size) { mutableMapOf() }
-	for (i in queries.indices) {
-		for (j in documents.indices) {
-			val document = documents[j]
-			val queryWords = queries[i].split(" ")
-			var counter = 0
-			for (k in queryWords.indices) {
-				val word = queryWords[k]
-				counter += document.getOrDefault(word, 0)
-			}
-			result[i][j] = counter
-		}
-	}
-  return result
-}
-
-fun documentRelevance2(queries: List<String>, documents: List<Map<String, Int>>): List<List<Pair<Int, Int>>> {
-	if (documents.isEmpty() || queries.isEmpty()) return emptyList()
-
-	val result = MutableList<MutableList<Pair<Int, Int>>>(queries.size) { mutableListOf() }
-	for (i in queries.indices) {
-		for (j in documents.indices) {
-			val document = documents[j]
-			val queryWords = queries[i].split(" ")
-			var counter = 0
-			for (k in queryWords.indices) {
-				val word = queryWords[k]
-				counter += document.getOrDefault(word, 0)
-			}
-			result[i].add(j to counter)
-		}
-	}
-	return result
-}
-
-fun countForSameWordsInLine(line: String): Map<String, Int> {
-	val result = mutableMapOf<String, Int>()
-
-	var i = 0
-	var j = 0
-	while (i <= line.lastIndex) {
-		while (j != line.lastIndex + 1 && line[j] != ' ') j++
-
-		val cur = line.substring(i, j)
-		result.compute(cur) { _, quantity ->
-			quantity?.let { it + 1 } ?: 1
-		}
-		j++
-		i = j
-	}
-
-	return result
-}
-
-private data class Data(
-	val serialNumberInDb: Int,
-	val indexes: List<Int>
+data class DocumentData(
+	val serialNumber: Int,
+	val relevanceValue: Int,
 )
