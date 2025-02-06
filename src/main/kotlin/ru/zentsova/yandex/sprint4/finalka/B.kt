3fbe44b7d069c27829a2ef0cd423f34328e3d7ac
Anastasiia Zentsova <pestovnikova2013@gmail.com>
2025-02-06 23:45:02 +0300

    sprint 4 - finalka A

--- src/main/kotlin/ru/zentsova/yandex/sprint4/finalka/B.kt
+++ src/main/kotlin/ru/zentsova/yandex/sprint4/finalka/B.kt
@@ -2,17 +2,18 @@ package ru.zentsova.yandex.sprint4.finalka
 
 /*
 -- Спринт 4. Финалка. B. Хеш-таблица --
-Ссылка на удачную посылку: https://contest.yandex.ru/contest/24414/run-report/132440959/
+Ссылка на удачную посылку: https://contest.yandex.ru/contest/24414/run-report/132440959/,
+  после 1-го ревью: https://contest.yandex.ru/contest/24414/run-report/132963835/
 
 -- ПРИНЦИП РАБОТЫ --
 Так как в условии сказано, что поддерживать рехеширование и масштабирование хеш-таблицы не требуется,
-то в качетсве размера массива я сразу выбрала первое ближайшее простое число (10^5 + 3) к заданному ограничению (10^5).
+то в качестве размера массива я сразу выбрала первое ближайшее простое число
+к удвоенному заданному ограничению (10^5) = 200003
 И размер у меня не изменяется.
 
-В качестве метода для разрешения коллизий явыбрала - метод цепочек.
+В качестве метода для разрешения коллизий я выбрала - метод цепочек.
 
-Функция хеширования принимает на вход значение ключа и применяет побитовые сдвиги
-и исключающее ИЛИ для равномерности распределения.
+Функция хеширования возвращает число, переданное на вход.
 
 -- ДОКАЗАТЕЛЬСТВО КОРРЕКТНОСТИ --
 Для всех методов вычисление индекса в массиве происходит одинаково:
@@ -20,10 +21,10 @@ package ru.zentsova.yandex.sprint4.finalka
 2. Значение из п.1 деление по модулю на размер массива
 
 Метод PUT:
-- Если по вычисленному индексу список не пустой, то в цикле сравниваются элементы по ключу.
-  Если ключ не найден, то в конец списка кладется значение (key, value).
+- Если по вычисленному индексу связанный список не пустой, то в цикле сравниваются элементы по ключу.
+  Если ключ не найден, то в конец связанного списка кладется значение (key, value).
   Если по ключу существует запись, то обновляется у текущей записи значение value.
-- Иначе, создается список из одного элемента (key, value).
+- Иначе, создается связанный список из одного элемента (key, value).
 
 Метод GET:
 - Как и в методе PUT ищется по ключу элемент, который необходимо вернуть,
@@ -38,7 +39,7 @@ package ru.zentsova.yandex.sprint4.finalka
 
 В среднем O(1)
 В худшем случае O(N), где N размер списка в бакете.
-Иногда из-за неизбежных коллизий по ключу в бакете лежит список и его необходимо обойти, чтобы найти элемент по ключу.
+Иногда из-за неизбежных коллизий по ключу в бакете лежит связанный список и его необходимо обойти, чтобы найти элемент по ключу.
 
 -- ПРОСТРАНСТВЕННАЯ СЛОЖНОСТЬ --
 
@@ -46,118 +47,73 @@ package ru.zentsova.yandex.sprint4.finalka
 */
 
 import java.io.BufferedReader
-import java.lang.Math.floorMod
-import java.util.*
+import java.util.LinkedList
 
 fun main() {
 	val reader = System.`in`.bufferedReader()
 	val operationsCount = reader.readInt()
 	val hashTable = HashTable()
-	for (i in 0 until operationsCount) {
-		val (operation, key, value) = reader.command()
-		when (operation) {
-			Operation.PUT -> hashTable.put(key, value!!)
-			Operation.GET -> println(hashTable.get(key) ?: "None")
-			Operation.DELETE -> println(hashTable.delete(key) ?: "None")
+	val outputBuffer = buildString {
+		for (i in 0 until operationsCount) {
+			val (operation, key, value) = reader.command()
+			when (operation) {
+				Operation.PUT -> hashTable.put(key, value!!)
+				Operation.GET -> append(hashTable.get(key) ?: "None").append("\n")
+				Operation.DELETE -> append(hashTable.delete(key) ?: "None").append("\n")
+			}
 		}
 	}
+
+	println(outputBuffer)
 }
 
 private fun BufferedReader.readInt() = readLine().toInt()
 
 private fun BufferedReader.command(): Triple<Operation, Int, Int?> {
-	val tokenizer = StringTokenizer(readLine())
+	val inputs = readLine().split(" ")
 
-	val operation = tokenizer.nextToken().toOperation()
-	val key = tokenizer.nextToken().toInt()
-	val value = if (tokenizer.hasMoreTokens()) tokenizer.nextToken().toInt() else null
+	val operation = Operation.fromString(inputs.first())
+	val key = inputs[1].toInt()
+	val value = inputs.getOrNull(2)?.toInt()
 
 	return Triple(operation, key, value)
 }
 
-private fun String.toOperation(): Operation = when (this) {
-	"put" -> Operation.PUT
-	"get" -> Operation.GET
-	"delete" -> Operation.DELETE
-	else -> throw IllegalArgumentException("Unknown operation")
-}
-
 class HashTable {
-	private val size: Int = 100003
-	private val table = Array<MutableList<KeyValue>?>(size) { null }
+	private val size: Int = 200003
+	private val table = Array<LinkedList<KeyValue>>(size) { LinkedList() }
 
 	fun put(key: Int, value: Int) {
 		val idx = bucketIndex(key)
-		val list = table[idx]
 
-		var shouldAdd = true
-		if (list != null) {
-			for (i in list.indices) {
-				if (list[i].key == key) {
-					shouldAdd = false
-					break
-				}
-			}
-			if (shouldAdd) table[idx]!!.add(KeyValue(key, value))
-		} else {
-			table[idx] = mutableListOf(KeyValue(key, value))
-		}
+		table[idx]
+			.firstOrNull { it.key == key }
+			?.let { it.value = value }
+			?: table[idx].add(KeyValue(key, value))
 	}
 
 	fun get(key: Int): Int? {
 		val idx = bucketIndex(key)
-		val list = table[idx]
-
-		return if (list != null) {
-			for (i in list.indices) {
-				if (list[i].key == key) {
-					return list[i].value
-				}
-			}
-			null
-		} else null
+		return table[idx].firstOrNull { it.key == key }?.value
 	}
 
 	fun delete(key: Int): Int? {
 		val idx = bucketIndex(key)
-		val list = table[idx]
+		val iterator = table[idx].iterator()
 
-		return if (list != null) {
-			var idxToDelete = -1
-			for (i in list.indices) {
-				if (list[i].key == key) {
-					idxToDelete = i
-					break
-				}
+		while (iterator.hasNext()) {
+			val entry = iterator.next()
+			if (entry.key == key) {
+				iterator.remove()
+				return entry.value
 			}
-
-			val valueToReturn = if (idxToDelete != -1) {
-				val value = list[idxToDelete].value
-				list.removeAt(idxToDelete)
-				value
-			} else null
-
-			valueToReturn
-		} else null
+		}
+		return null
 	}
 
-	private fun hashCode(k: Int): Int {
-		val prime = 31
-		var result = k
+	private fun hashCode(key: Int) = key
 
-		result = result xor (result ushr 16)
-		result *= prime
-		result = result xor (result ushr 13)
-		result *= prime
-		result = result xor (result ushr 14)
-
-		return result
-	}
-
-	private fun bucketIndex(key: Int): Int {
-		val hash = hashCode(key)
-		return floorMod(hash, size)
-	}
+	private fun bucketIndex(key: Int) = hashCode(key).mod(size)
 
 	private data class KeyValue(
 		val key: Int,
@@ -166,5 +122,12 @@ class HashTable {
 }
 
 enum class Operation {
-	PUT, GET, DELETE,
+	PUT, GET, DELETE;
+
+	companion object {
+		private val map = Operation.values().associateBy { it.name.lowercase() }
+
+		fun fromString(value: String): Operation =
+			map[value] ?: throw IllegalArgumentException("Unknown operation: $value")
+	}
 }
