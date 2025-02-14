f01deed42daa0e91a0e80feab2db85ddd91d83ce
Anastasia Zentsova <a.zentsova@lipt-soft.ru>
2025-02-14 18:41:52 +0300

    sprint 5 - A

--- src/main/kotlin/ru/zentsova/yandex/sprint5/A.kt
+++ src/main/kotlin/ru/zentsova/yandex/sprint5/A.kt
@@ -1,5 +1,6 @@
 package ru.zentsova.yandex.sprint5
 
+import java.util.*
 import kotlin.math.max
 
 // <template>
@@ -10,25 +11,32 @@ class Node(var value: Int) {
 // <template>
 
 fun treeSolution(head: Node?): Int {
-	if (head == null) return 0
+	if (head == null) return -1
 
-	val checkedVertices = mutableSetOf(1)
-  var maxValue = head.value
-	var left = head.left
-	while (left != null) {
-		maxValue = max(left.value, maxValue)
-		checkedVertices.add(left.value)
-		left = left.left
+	val stack = Stack<Node>()
+	stack.push(head)
+	var maxValue = head.value
+	var next = head.left
+	while (stack.isNotEmpty() || next != null) {
+		while (next != null && !stack.contains(next)) {
+			maxValue = max(next.value, maxValue)
+			stack.push(next)
+			next = next.left
+		}
+
+		next = if (stack.size > 0) stack.pop().right else null
 	}
+
+	return maxValue
 }
 
 fun test() {
 	val node1 = Node(1)
 	val node2 = Node(-5)
 	val node3 = Node(3)
-	node3?.left = node1
-	node3?.right = node2
+	node3.left = node1
+	node3.right = node2
 	val node4 = Node(2)
-	node4?.left = node3
+	node4.left = node3
 	assert(treeSolution(node4) == 3)
 }
---
+++ src/test/kotlin/ru/zentsova/yandex/sprint5/AKtTest.kt
@@ -0,0 +1,41 @@
+package ru.zentsova.yandex.sprint5
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class AKtTest {
+
+  @Test
+  fun treeSolutionTest() {
+//    val head = Node(1)
+//    val node2 = Node(3)
+//    val node3 = Node(5)
+//    val node4 = Node(8)
+//    val node5 = Node(10)
+//
+//    head.left = node2
+//    head.right = node3
+//
+//    node2.left = node4
+//    node2.right = node5
+//
+//    assertThat(treeSolution(head)).isEqualTo(10)
+
+//    val node1 = Node(1)
+//    val node2 = Node(-5)
+//    val node3 = Node(3)
+//    node3?.left = node1
+//    node3?.right = node2
+//    val node4 = Node(2)
+//    node4?.left = node3
+//
+//    assertThat(treeSolution(node4)).isEqualTo(3)
+
+    val head = Node(1)
+    val node2 = Node(2)
+
+    head.right = node2
+
+    assertThat(treeSolution(head)).isEqualTo(2)
+  }
+}
