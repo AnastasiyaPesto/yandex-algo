c3afec3717de2a6128b5f7ded1d47f2ab66fc21a
Anastasiia Zentsova <a.zentsova@lipt-soft.ru>
2025-04-16 18:10:15 +0300

    sprint 6 - finalka (A, B)

--- src/main/kotlin/ru/zentsova/yandex/sprint6/finalka/A.kt
+++ src/main/kotlin/ru/zentsova/yandex/sprint6/finalka/A.kt
@@ -2,19 +2,19 @@ package ru.zentsova.yandex.sprint6.finalka
 
 /*
 -- Спринт 6. Финалка. A. Дорогая сеть --
-Ссылка на удачную посылку: https://contest.yandex.ru/contest/25070/run-report/136811848/
+Ссылка на удачную посылку: https://contest.yandex.ru/contest/25070/run-report/136850653/
 
 -- ПРИНЦИП РАБОТЫ --
 Алгоритм:
 1. Считывание данных (читываю через BufferedReader):
 Заполняю структуру данных - graph: Array<List<Edge>>:
 
-Список ребер (ребро в формате - конечная веришна + вес ребра).
+Список ребер (ребро в формате - конечная вершина + вес ребра).
 Начало ребра это первый индекс массива.
 
 2. fun maxSpanningTree(graph)
 Начинаю с вершины 0. Помечаю ее как посещенную. Для этого заведен массив visited: Array<Boolean>.
-Здесь же завожу счетчик (visitedCount), чтобы считать кол-во посещеных вершин, это поможет за константное время сделать вывод
+Здесь же завожу счетчик (visitedCount), чтобы считать кол-во посещенных вершин, это поможет за константное время сделать вывод
 о количестве компонент связности.
 Все рёбра, исходящие из вершины 0 добавляю в бинарную кучу на максимум в формате пары - конечная вершина и вес.
 Пользуюсь очередью из стандартной библиотеки - PriorityQueue.
@@ -24,7 +24,7 @@ package ru.zentsova.yandex.sprint6.finalka
 2.2. Если конечная вершина (V), к которой ведёт ребро, ещё не посещена:
 2.2.1. Помечаю ее как посещённую.
 2.2.2. Прибавляю вес W к общему весу дерева (totalWeight).
-2.2.3. Все рёбра из V к непосещённым вершинам добавляю в кучу.
+2.2.3. Все рёбра из V к не посещённым вершинам добавляю в кучу.
 2.3. Последним шагом сравниваю visitedCount и исходное число вершин.
 Если граф имеет 1 компоненту связанности, то visitedCount равно vertexCount => вывожу значение  totalWeight,
 иначе в графе более одной компоненты связности.
@@ -37,75 +37,70 @@ package ru.zentsova.yandex.sprint6.finalka
 -- ВРЕМЕННАЯ СЛОЖНОСТЬ --
 O(∣E∣⋅log∣V∣), где ∣E∣ — количество рёбер в графе, а ∣V∣ — количество вершин.
 
-Тут мне было не супер понятно, но смею предоложить, что зависимость от вершин (несмотря на то, что в куче хранятся ребра),
+Тут мне было не супер понятно, но смею предположить, что зависимость от вершин (несмотря на то, что в куче хранятся ребра),
 потому что каждый раз мы ищем ребра, которые исходят из каждой вершины.
 Обрабатываем вершину один раз, а потом добавляем все ребра, исходящие из нее и работаем с ними.
 
 -- ПРОСТРАНСТВЕННАЯ СЛОЖНОСТЬ --
-O(∣V∣) - массив для учета посещенных вершин (visited), где ∣V∣ — количество вершин в графе.
-O(|E|) - приоритетная очеред, где ∣E∣ — количество рёбер в графе
-O(|V|^2) - матрица смежностей (edgesWeight)
-O(|V| + |E|) - список смежностей
+O(|E|) - приоритетная очередь, где ∣E∣ — количество рёбер в графе
+O(|V| + |E|) - массив списков ребер (где сразу помечаем посещенные вершины)
 
-Итог: O(|V|^2), где ∣V∣ — количество вершин в графе.
+Итог: O(|V| + |E|), где ∣V∣ — количество вершин в графе, ∣E∣ - количество ребер в графе
 */
 
 import java.io.BufferedReader
 import java.util.*
 
 fun main() {
-	val reader = System.`in`.bufferedReader()
-	val (vertexCount, edgeCount) = reader.readVertexAndEdgeCount()
+  val reader = System.`in`.bufferedReader()
+  val (vertexCount, edgeCount) = reader.readInts()
 
-	val graph = reader.readGraph(vertexCount, edgeCount)
-	val maxWeight = maxSpanningTree(graph)
-	println(maxWeight ?: "Oops! I did it again")
+  val graph = reader.readGraph(vertexCount, edgeCount)
+  val maxWeight = maxSpanningTree(graph)
+  println(if (maxWeight == -1) "Oops! I did it again" else maxWeight)
 }
 
-fun maxSpanningTree(graph: Array<MutableList<Edge>>): Int? {
-	var totalWeight = 0
-	val visited = BooleanArray(graph.size)
-	val heap = PriorityQueue<Edge>(compareByDescending { it.weight })
-	var visitedCount = 0
+fun maxSpanningTree(graph: Array<MutableList<Edge>>): Int {
+  var totalWeight = 0
+  val visited = BooleanArray(graph.size)
+  val heap = PriorityQueue<Edge>(compareByDescending { it.weight })
+  var visitedCount = 0
 
-	fun visitVertex(v: Int) {
-		visited[v] = true
-		visitedCount++
-		graph[v].forEach { edge ->
-			if (!visited[edge.to]) {
-				heap.add(edge)
-			}
-		}
-	}
+  fun visitVertex(v: Int) {
+    visited[v] = true
+    visitedCount++
+    graph[v].forEach { edge ->
+      if (!visited[edge.to]) {
+        heap.add(edge)
+      }
+    }
+  }
 
-	visitVertex(0)
-	while (heap.isNotEmpty()) {
-		val edge = heap.poll()
-		if (!visited[edge.to]) {
-			totalWeight += edge.weight
-			visitVertex(edge.to)
-		}
-	}
+  visitVertex(0)
+  while (heap.isNotEmpty()) {
+    val edge = heap.poll()
+    if (!visited[edge.to]) {
+      totalWeight += edge.weight
+      visitVertex(edge.to)
+    }
+  }
 
-	return if (visitedCount != graph.size) null else totalWeight
+  return if (visitedCount != graph.size) -1 else totalWeight
 }
 
 private fun BufferedReader.readInts() = readLine().split(" ").map(String::toInt)
 
-private fun BufferedReader.readVertexAndEdgeCount(): Pair<Int, Int> =
-	readInts().let { it.first() to it.last() }
-
 private fun BufferedReader.readGraph(vertexCount: Int, edgeCount: Int): Array<MutableList<Edge>> {
-	val graph = Array(vertexCount) { mutableListOf<Edge>() }
+  val graph = Array(vertexCount) { mutableListOf<Edge>() }
 
-	repeat(edgeCount) {
-		val (from, to, weight) = readInts()
+  repeat(edgeCount) {
+    val (from, to, weight) = readInts()
 
-		graph[from - 1].add(Edge(to - 1, weight))
-		graph[to - 1].add(Edge(from - 1, weight))
-	}
+    graph[from - 1].add(Edge(to - 1, weight))
+    graph[to - 1].add(Edge(from - 1, weight))
+  }
 
-	return graph
+  return graph
 }
 
 data class Edge(val to: Int, val weight: Int)
--- src/main/kotlin/ru/zentsova/yandex/sprint6/finalka/B.kt
+++ src/main/kotlin/ru/zentsova/yandex/sprint6/finalka/B.kt
@@ -14,7 +14,7 @@ package ru.zentsova.yandex.sprint6.finalka
 - Запоминаем его размер, если он больше предыдущего максимального
 
 2. fun dfs()
-- Завела массим в направлениями directions и сложив текущую точку со всем значениями - получаю всех соседей
+- Завела массив в направлениями directions и сложив текущую точку со всем значениями - получаю всех соседей
 - Пока есть что обходить — достаём вершину из стека
 - Обходим всех четырёх соседей (с проверкой границ и что они — земля и не посещены)
 - Помечаем как посещённые и добавляем в стек
@@ -35,74 +35,77 @@ O(n * m) — где n * m - размер карты (хранение карты
 import java.io.BufferedReader
 
 fun main() {
-	val reader = System.`in`.bufferedReader()
-	val (n, _) = reader.readInts()
-	val map = Array(n) { reader.readInputData() }
+  val reader = System.`in`.bufferedReader()
+  val (n, m) = reader.readInts()
+  val map = Array(n) { reader.readInputData(m) }
 
-	val (islandCount, maxSize) = mainDFS(map)
+  val (islandCount, maxSize) = mainDFS(map)
 
-	println("$islandCount $maxSize")
+  println("$islandCount $maxSize")
 }
 
 fun mainDFS(map: Array<BooleanArray>): Pair<Int, Int> {
-	val n = map.size
-	val m = map.first().size
+  val n = map.size
+  val m = map.first().size
 
-	var islandCount = 0
-	var maxSize = 0
+  var islandCount = 0
+  var maxSize = 0
 
-	for (x in 0 until n) {
-		for (y in 0 until m) {
-			val point = Point(x, y)
-			if (map[point]) {
-				val size = dfs(map, point)
-				islandCount++
-				maxSize = maxOf(maxSize, size)
-			}
-		}
-	}
+  for (x in 0 until n) {
+    for (y in 0 until m) {
+      val point = Point(x, y)
+      if (map[point]) {
+        val size = dfs(map, point)
+        islandCount++
+        maxSize = maxOf(maxSize, size)
+      }
+    }
+  }
 
-	return islandCount to maxSize
+  return islandCount to maxSize
 }
 
 fun dfs(map: Array<BooleanArray>, start: Point): Int {
-	val directions = listOf(
-		Point(-1, 0), Point(1, 0),
-		Point(0, -1), Point(0, 1)
-	)
+  val directions = listOf(
+    Point(-1, 0), Point(1, 0),
+    Point(0, -1), Point(0, 1)
+  )
 
-	val stack = ArrayDeque<Point>()
-	stack.add(start)
-	map[start] = false
-	var size = 1
+  val stack = ArrayDeque<Point>()
+  stack.add(start)
+  map[start] = false
+  var size = 1
 
-	val n = map.size
-	val m = map[0].size
+  val n = map.size
+  val m = map[0].size
 
-	while (stack.isNotEmpty()) {
-		val current = stack.removeLast()
-		for (dir in directions) {
-			val neighbor = current + dir
-			if (neighbor.isInBounds(n, m) && map[neighbor]) {
-				map[neighbor] = false
-				stack.add(neighbor)
-				size++
-			}
-		}
-	}
+  while (stack.isNotEmpty()) {
+    val current = stack.removeLast()
+    for (dir in directions) {
+      val neighbor = current + dir
+      if (neighbor.isInBounds(n, m) && map[neighbor]) {
+        map[neighbor] = false
+        stack.add(neighbor)
+        size++
+      }
+    }
+  }
 
-	return size
+  return size
 }
 
 private fun BufferedReader.readInts() = readLine().split(" ").map(String::toInt)
-private fun BufferedReader.readInputData() = readLine().toCharArray().map { it == '#' }.toBooleanArray()
+private fun BufferedReader.readInputData(size: Int): BooleanArray {
+  val line = readLine()
+  return BooleanArray(size) { line[it] == '#' }
+}
 
 data class Point(val x: Int, val y: Int) {
-	operator fun plus(other: Point) = Point(x + other.x, y + other.y)
-	fun isInBounds(xOther: Int, yOther: Int) = x in 0 until xOther && y in 0 until yOther
+  operator fun plus(other: Point) = Point(x + other.x, y + other.y)
+  fun isInBounds(xOther: Int, yOther: Int) = x in 0 until xOther && y in 0 until yOther
 }
 
 operator fun Array<BooleanArray>.get(p: Point): Boolean = this[p.x][p.y]
 operator fun Array<BooleanArray>.set(p: Point, value: Boolean) {
-	this[p.x][p.y] = value
+  this[p.x][p.y] = value
 }
